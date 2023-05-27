579b7cbe60879eb40bed16cff8ab459946915890
Joshua George <horheyinc8@gmail.com>
2023-05-28 00:10:29 +0200

    dark theme

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,19 +16,18 @@ export default function Contact() {
           <img src={personalPic} alt="personal picture" className="about-pic" />
           <section>
             <Typography sx={{ mb: 2 }} color="white" variant="h6">
-              -Proud graduate of Ironhack Full-Stack Web Development
-              Bootcamp.(remote)
+              -Ironhack Full-Stack Web Development Bootcamp Graduate (remote)
             </Typography>
             <Typography sx={{ mb: 2 }} color="white" variant="h6">
               -Freelancer / Web Dev Lead Teacher with a passion for learning new
               tech stacks.
             </Typography>
             <Typography sx={{ mb: 2 }} color="white" variant="h6">
-              -Specializing in HTML, CSS, Javascript and React, with experience
-              in Next-JS and Vite.
+              -Specializing in HTML, CSS, Javascript and React with experience
+              in Next-JS, Typescript and Vite.
             </Typography>
             <Typography sx={{ mb: 2 }} color="white" variant="h6">
-              -2 years of educating future MERN developers. I have facilited,
+              -2 years of educating future MERN developers where I facilited,
               conceptualized, debugged and help deploy over 200 team projects.
             </Typography>
             <Typography sx={{ mb: 2 }} color="white" variant="h6">
@@ -41,10 +40,3 @@ export default function Contact() {
     </div>
   );
 }
-// export default function Contact() {
-//   return (
-//     <Box id="experience-card">
-//       <Card variant="outlined">{card}</Card>
-//     </Box>
-//   );
-// }

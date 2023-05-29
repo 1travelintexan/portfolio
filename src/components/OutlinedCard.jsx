15ff5a9b3426bfaf5eedf94d4a1b7129ee434de2
Joshua George <horheyinc8@gmail.com>
2023-05-29 17:45:44 +0200

    hondee changes

--- src/components/OutlinedCard.jsx
+++ src/components/OutlinedCard.jsx
@@ -5,11 +5,15 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
 const card = (
-  <div>
-    <CardContent>
-      <Typography sx={{ mb: 1, mt: 2 }} color="text.secondary" variant="h5">
-        Web Dev Lead Teacher(January/2023 - Present)
+  <>
+    <CardContent style={{ background: "rgb(255, 255, 255)" }}>
+      <Typography sx={{ mb: 0 }} color="text.secondary" variant="h5">
+        Web Dev Lead Teacher
+      </Typography>
+      <Typography sx={{ mb: 1 }} color="text.secondary" variant="h6">
+        (January/2023 - Present)
       </Typography>
       <Typography sx={{ mb: 1 }} color="text.secondary" variant="h6">
         Daily requirements
@@ -30,8 +34,11 @@ const card = (
         -Above all, my job is to make sure the students are happy, learning and
         feel that they are getting the very best out of the course.
       </Typography>
-      <Typography sx={{ mb: 1, mt: 3 }} color="text.secondary" variant="h5">
-        Web Dev Teachers Assistant(May/2021-January/2023)
+      <Typography sx={{ mt: 4 }} color="text.secondary" variant="h5">
+        Web Dev Teachers Assistant
+      </Typography>
+      <Typography sx={{ mb: 1 }} color="text.secondary" variant="h6">
+        (May/2021-Jan/2023)
       </Typography>
       <Typography sx={{ mb: 1 }} color="text.secondary" variant="h6">
         Daily requirements
@@ -44,12 +51,15 @@ const card = (
         -Evaluate student code for common mistakes and recommended best
         practices for improvement.
       </Typography>
-      <Typography sx={{ mb: 3 }}>
+      <Typography sx={{ mb: 1 }}>
         -Approve ideas for student project ideas, then help them organize and
         structure their code to create the app of their dreams.
       </Typography>
-      <Typography sx={{ mb: 1, mt: 3 }} color="text.secondary" variant="h5">
-        Ironhack Full-Stack Web Development Bootcamp Graduate (2020)
+      <Typography sx={{ mb: 0, mt: 3 }} color="text.secondary" variant="h5">
+        Ironhack Full-Stack Web Development Bootcamp Graduate
+      </Typography>
+      <Typography color="text.secondary" variant="h6">
+        (2020)
       </Typography>
       <Typography sx={{ mb: 3 }}>
         -9 week rigorous bootcamp that focuses on challenging students to
@@ -59,17 +69,17 @@ const card = (
       </Typography>
     </CardContent>
     <CardActions>
-      <a href="https://www.linkedin.com/in/-joshua-george/" target="_blank">
-        <Button size="small">LinkedIn</Button>
-      </a>
+      <Link href="https://www.linkedin.com/in/-joshua-george/" target="_blank">
+        <Button>LinkedIn</Button>
+      </Link>
     </CardActions>
-  </div>
+  </>
 );
 
 export default function OutlinedCard() {
   return (
     <>
-      <Typography variant="h3" component="div" sx={{ m: 1 }} color="white">
+      <Typography variant="h3" component="div" sx={{ mt: 3 }} color="white">
         Experience:
       </Typography>
       <Box sx={{ minWidth: 275 }} id="experience-card">

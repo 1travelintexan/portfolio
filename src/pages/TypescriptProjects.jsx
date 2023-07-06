06afa390df27dbdbebe6a44f82b37ae655ef97ff
Joshua <horheyinc8@gmail.com>
2023-07-06 17:33:50 +0200

    img update, and typescript

--- src/pages/TypescriptProjects.jsx
+++ src/pages/TypescriptProjects.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import MediaCard from "../components/MediaCard";
+import kindleflixImg from "../assets/kindleflix.png";
 
 function TypescriptProjects() {
-  return <div>TypescriptProjects</div>;
+  return (
+    <div className="projects-page">
+      <div className="projects-header">
+        <article>Typescript Projects</article>
+      </div>
+      <div className="projects-container">
+        <MediaCard
+          image={kindleflixImg}
+          info={[
+            "Full-Stack(MERN)",
+            "Typescript",
+            "Auth",
+            "React Hooks",
+            "Express",
+            "MongoDB",
+          ]}
+          title="Kindleflix"
+          url="https://read-a-book-and-chill.netlify.app/"
+          github="https://github.com/1travelintexan/read-more-client"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default TypescriptProjects;
